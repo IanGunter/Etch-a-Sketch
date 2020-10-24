da07dda1ea97035d7a43963229be0c5cd3ebf9a4
Ian Gunter <iangunterofamerica@yahoo.com>
2020-10-24 17:13:48 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DrawBox = New System.Windows.Forms.PictureBox()
+        Me.ChooseColor = New System.Windows.Forms.ColorDialog()
+        Me.ChooseColorButton = New System.Windows.Forms.Button()
+        CType(Me.DrawBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DrawBox
+        '
+        Me.DrawBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.DrawBox.Location = New System.Drawing.Point(119, 33)
+        Me.DrawBox.Name = "DrawBox"
+        Me.DrawBox.Size = New System.Drawing.Size(579, 299)
+        Me.DrawBox.TabIndex = 0
+        Me.DrawBox.TabStop = False
+        '
+        'ChooseColorButton
+        '
+        Me.ChooseColorButton.Location = New System.Drawing.Point(138, 356)
+        Me.ChooseColorButton.Name = "ChooseColorButton"
+        Me.ChooseColorButton.Size = New System.Drawing.Size(123, 50)
+        Me.ChooseColorButton.TabIndex = 1
+        Me.ChooseColorButton.Text = "Choose Color"
+        Me.ChooseColorButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ChooseColorButton)
+        Me.Controls.Add(Me.DrawBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DrawBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DrawBox As PictureBox
+    Friend WithEvents ChooseColor As ColorDialog
+    Friend WithEvents ChooseColorButton As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+
+    Dim drawPen As New System.Drawing.Pen(Color.Black, 1)
+    Dim erasePen As New System.Drawing.Pen(Color.FromName("Control"), 10)
+    Dim g As System.Drawing.Graphics
+    Dim LastX As Integer
+    Dim LastY As Integer
+
+
+    Sub Draw(x As Integer, y As Integer)
+        g = DrawBox.CreateGraphics
+        If LastX = Nothing Then
+            g.DrawLine(drawPen, x, y, x, y)
+
+        Else
+            g.DrawLine(drawPen, x, y, LastX, LastY)
+
+
+        End If
+        LastX = x
+        LastY = y
+
+
+    End Sub
+
+    Sub ColorChooser()
+        ChooseColor.ShowDialog()
+        drawPen.Color = ChooseColor.Color
+    End Sub
+
+
+    Sub Eraser(x As Integer, y As Integer)
+        g = DrawBox.CreateGraphics()
+        g.DrawLine(erasePen, x, y, x - 1, y - 1)
+    End Sub
+
+    Private Sub DrawBox_MouseHoldMove(sender As Object, e As MouseEventArgs) Handles DrawBox.MouseDown, DrawBox.MouseMove
+        If e.Button.ToString = "Left" Then
+            Draw(e.X, e.Y)
+        ElseIf e.Button.ToString = "Right" Then
+            Eraser(e.X, e.Y)
+        ElseIf e.Button.ToString = "Middle" Then
+            ColorChooser()
+        End If
+    End Sub
+
+    Private Sub ChooseColorButton_Click(sender As Object, e As EventArgs) Handles ChooseColorButton.Click
+        ColorChooser()
+    End Sub
+End Class
